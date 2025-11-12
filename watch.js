63d0e4d39c0149b2c71ea2395767f55ddd5e5d61
Vered-Ben-David <vered054819@gmail.com>
2025-11-12 12:22:38 +0200

    Update watch.js

--- watch.js
+++ watch.js
@@ -1,1 +1,16 @@
+(function () {
+  const dbg = null;
+  const log = (msg) => { console.log('[watch]', msg); };
+
+  log('watch.js LOADED v5');
+
+  const qs = new URLSearchParams(location.search);
+  const titleId = qs.get('id') || '';
+  const v = document.getElementById('player');
+
+  log('titleId=' + titleId);
+  log('has <video id="player"> = ' + !!v);
+
+  if (!titleId) { alert('חסר מזהה כותרת'); location.href = '/app'; return; }
+})();
 
