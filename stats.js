265dc5a673be264bd66565d291190ee5af5e32d2
Vered-Ben-David <vered054819@gmail.com>
2025-11-13 17:14:22 +0200

    Update stats.js

--- stats.js
+++ stats.js
@@ -18,8 +18,6 @@ async function getMeStats() {
     '<li class="nav-item"><a class="nav-link" href="/profiles">' +
     prof +
     '</a></li>' +
-    '<li class="nav-item"><a class="nav-link" href="/stats.html">סטטיסטיקות</a></li>' +
-    '<li class="nav-item"><a class="nav-link" href="/admin.html">אדמין</a></li>' +
     '<li class="nav-item"><a class="nav-link" href="#" id="logout">יציאה</a></li>';
 
   document.getElementById("logout").onclick = async () => {
